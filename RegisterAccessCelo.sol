6451102b84151525e37d1135d252ba0f01061af8
Andrew Kim Joseph <91619206+andrewkimjoseph@users.noreply.github.com>
2024-04-13 19:07:26 +0300

    Update RegisterAccessCelo.sol

--- RegisterAccessCelo.sol
+++ RegisterAccessCelo.sol
@@ -1,58 +1,123 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
- 
-contract RegisterAccessCelo {
-        string[] private info;
-        address public owner;
-        mapping (address => bool) public allowlist;
- 
-        constructor() {
-            owner = msg.sender;
-            allowlist[msg.sender] = true;
-        } 
- 
-        event InfoChange(string oldInfo, string newInfo); 
- 
-        modifier onlyOwner {
-            require(msg.sender == owner,"Only owner");
-            _;
-        }
- 
-        modifier onlyAllowlist {
-            require(allowlist[msg.sender] == true, "Only allowlist");
-            _;
-        } 
- 
-        function getInfo(uint index) public view returns (string memory) {
-            return info[index];
-        }
- 
-        function setInfo(uint index, string memory _info) public onlyAllowlist {
-            emit InfoChange (info[index], _info);
-            info[index] = _info;
-        }
- 
-        function addInfo(string memory _info) public onlyAllowlist returns (uint index) {
-            info.push(_info);
-            index = info.length - 1;
-        }
-   
-        function listInfo() public view returns (string[] memory) {
-            return info;
-        }
- 
-        function addMember (address _member) public onlyOwner {        
-            allowlist[_member] = true;
-        }
- 
-        function delMember (address _member) public onlyOwner {
-            allowlist[_member] = false;
-        }    
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+struct Contributor {
+    address id;
+    string username;
 }
 
+struct Saving {
+    uint256 id;
+    address creator;
+    address[4] contributors;
+    uint256[4] contributions;
+    uint256 currentRecipientIndex;
+}
 
-// Network Deployed: Celo Alfajores
-// Address Deployed:
-// 1. 0xDE2116AB07393F2F03B5dB3dc6aB3946a488641a
+enum TransactionNature {
+    DEPOSIT,
+    WITHDRAWAL
+}
 
-// 0x0Ea89A815b8fCB113FC7525F1e720CEF392ceFdB
+struct Transaction {
+    Contributor contributor;
+    Saving saving;
+    TransactionNature nature;
+}
+
+contract PamojaAppSavingsContract {
+    Contributor[] public contributors;
+    mapping(address => bool) public contributorExistsInContributors;
+    uint256 public savingId;
+
+    Saving[] public savings;
+    mapping(address => bool) public contributorExistsInSavings;
+
+    IERC20 CUSDAlfajoresContract =
+        IERC20(0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1);
+
+    function addContributor(string memory username) public {
+        if (contributorExistsInContributors[msg.sender]) {
+            revert("Contributor already exists.");
+        }
+        contributors.push(Contributor(msg.sender, username));
+    }
+
+    function getContributor(address id)
+        public
+        view
+        returns (Contributor memory)
+    {
+        if (!contributorExistsInContributors[msg.sender]) {
+            revert("Contributor does not exist.");
+        }
+
+        uint256 locationIndex = 0;
+
+        for (uint256 index = 0; index < contributors.length; index++) {
+            if (contributors[index].id == id) {
+                locationIndex = index;
+            }
+        }
+
+        return contributors[locationIndex];
+    }
+
+    function createSaving(uint256 contribution) public {
+        bool success = CUSDAlfajoresContract.transfer(
+            address(this),
+            contribution
+        );
+
+        if (!success) {
+            revert("Savings creation failed.");
+        }
+
+        Saving memory newSaving;
+        newSaving.id = savingId++;
+        newSaving.creator = msg.sender;
+        newSaving.contributors[0] = msg.sender;
+        newSaving.currentRecipientIndex = 0;
+        savings.push(newSaving);
+    }
+
+    function withdrawSaving(Saving memory saving, address recipient)
+        public
+        returns (bool)
+    {
+        if (msg.sender != saving.creator) {
+            revert("Only the creator of a saving can make a withdrawal.");
+        }
+
+        if (recipient == msg.sender && saving.currentRecipientIndex > 0) {
+            revert("It seems you already withdrew to your account.");
+        }
+
+        uint256 withdrawalAmount = 0;
+
+        for (uint256 index = 0; index < saving.contributions.length; index++) {
+            withdrawalAmount += saving.contributions[index];
+        }
+
+        bool success = CUSDAlfajoresContract.transfer(
+            address(this),
+            withdrawalAmount
+        );
+
+        if (success) {
+            for (
+                uint256 index = 0;
+                index < savings.length;
+                index++
+            ) {
+                if (saving.id ==savings[index].id){
+                    savings[index].currentRecipientIndex++
+                }
+            }
+        }
+
+        return success;
+    }
+}
